6fb83b55cb1805f52af4664ae339fc54046a136e
s_arme <s.arme@ya.ru>
2020-02-07 19:23:05 +0330

    Implement get_legal_moves that returns available moves for a player

--- gamestate.py
+++ gamestate.py
@@ -19,12 +19,71 @@ class GameState:
         -------
         None
         """
-        self._cells = [[0 for i in range(n)] for i in range(m)]
+        self._m = m  # row
+        self._n = n  # col
+        self._cells = [[-1 for i in range(n)] for i in range(m)]
         self._cells[-1][-1] = 1  # block down right for y
-        self._last_x = (0, 0)
+        self._last_x = (0, 0)  # row, col
         self._last_y = (0, 0)
         self._initiative = 0  # 0 for x, 1 for y
 
+    def get_legal_moves(self):
+        """ Return a list of all legal moves available to the
+        active player.  Each player should get a list of all
+        empty spaces on the board on their first move, and
+        otherwise they should get a list of all open spaces
+        in a straight line along any row, column or diagonal
+        from their current position. (Players CANNOT move
+        through obstacles or blocked squares.) Moves should
+        be a pair of integers in (column, row) order specifying
+        the zero-indexed coordinates on the board.
+        """
+        available = []
+        direction = [(1, 0), (0, 1), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1)]
+        if sum(self._last_x) == 0:
+            if self._initiative == 0:
+                for i in range(self._m):
+                    for j in range(self._n):
+                        if i == 1:
+                            continue
+                        available.append((i, j))
+        elif sum(self._last_y) == 0:
+            if self._initiative == 1:
+                for i in range(self._m):
+                    for j in range(self._n):
+                        if i == 0:
+                            continue
+                        available.append((i, j))
+        else:
+            if self._initiative == 1:
+                _x, _y = self._last_y
+                for i, j in direction:
+                    while 0 <= _x + i < self._m and 0 <= _y + i < self._n:
+                        if self._cells[_x][_y] == 1:
+                            break
+                        available.append((_x, _y))
+            if self._initiative == 0:
+                _x, _y = self._last_x
+                for i, j in direction:
+                    while 0 <= _x + i < self._m and 0 <= _y + i < self._n:
+                        if self._cells[_x][_y] == 0:
+                            break
+                        available.append((_x, _y))
+        return available
+
+    def forecast_move(self, move):
+        """ Return a new board object with the specified move
+        applied to the current game state.
+
+        Parameters
+        ----------
+        move: tuple
+            The target position for the active player's next move
+            (e.g., (0, 0) if the active player will move to the
+            top-left corner of the board)
+        """
+        # TODO: implement this function!
+        pass
 
 
 if __name__ == "__main__":
